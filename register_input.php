7f09da0cdc20a928d35b1478c958cb6cbba6c9d2
Karisna <kharisnarachmazaki19@gmail.com>
2023-08-15 19:12:15 +0700

    login

---
+++ register_input.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bedengan_3";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$nama = $_POST['nama'];
+$iduser = $_POST['iduser'];
+$telp = $_POST['telp'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO user (nama, iduser, telp, password)
+VALUES ('$nama','$iduser','$telp','$password')";
+
+$result  = mysqli_query($conn, $sql);
+
+if ($result)
+{
+    header("Location: http://localhost/bedengan_2/login.php");
+}
+else
+{
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?> 
